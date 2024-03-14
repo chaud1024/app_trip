567723e631343ea5e19c5142aecec1b7a77c6019
chaud1024 <chaud1024@gmail.com>
2024-03-14 17:54:12 +0900

    create test pages & test button

---
+++ src/components/test/HotelListAddButton.tsx
@@ -0,0 +1,7 @@
+import Button from '@shared/Button'
+
+const HotelListAddButton = () => {
+  return <Button>호텔리스트추가</Button>
+}
+
+export default HotelListAddButton
---
+++ src/pages/Test.tsx
@@ -0,0 +1,12 @@
+import HotelListAddButton from '@components/test/HotelListAddButton'
+
+const TestPage = () => {
+  return (
+    <div>
+      TestPage
+      <HotelListAddButton />
+    </div>
+  )
+}
+
+export default TestPage
