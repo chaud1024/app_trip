45b4a2464d5b650ffa312bb256f2a0fad7d6d5f3
chaud1024 <chaud1024@gmail.com>
2024-03-14 21:42:42 +0900

    add function to set the data into the firebase

--- src/components/test/HotelListAddButton.tsx
+++ src/components/test/HotelListAddButton.tsx
@@ -1,7 +1,44 @@
+import { collection, doc, writeBatch } from 'firebase/firestore'
+
+import { EVENTS, HOTEL, HOTEL_NAMES, IMAGES, ROOMS } from '@/mock/data'
+import { COLLECTIONS } from '@constants'
+import { store } from '@remote/firebase'
 import Button from '@shared/Button'
 
+function random(min: number, max: number) {
+  return Math.floor(Math.random() + (max - min + 1) + min)
+}
+
 const HotelListAddButton = () => {
-  return <Button>호텔리스트추가</Button>
+  const batch = writeBatch(store)
+  const handleButtonClick = () => {
+    const hotels = HOTEL_NAMES.map((hotelName, idx) => {
+      return {
+        name: hotelName,
+        mainImageUrl: IMAGES[Math.floor(Math.random() * IMAGES.length)],
+        images: IMAGES,
+        price: random(130000, 280000),
+        starRating: random(1, 5),
+        ...HOTEL,
+        ...(EVENTS[idx] != null && { events: EVENTS[idx] }),
+      }
+    })
+
+    hotels.forEach((hotel) => {
+      const hotelDocRef = doc(collection(store, COLLECTIONS.HOTEL))
+
+      batch.set(hotelDocRef, hotel)
+
+      ROOMS.forEach((room) => {
+        const subDocRef = doc(collection(hotelDocRef, COLLECTIONS.ROOM))
+        batch.set(subDocRef, room)
+      })
+    })
+
+    batch.commit()
+  }
+
+  return <Button onClick={handleButtonClick}>호텔리스트추가</Button>
 }
 
 export default HotelListAddButton
