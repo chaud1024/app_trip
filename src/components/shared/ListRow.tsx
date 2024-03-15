ba775cc7de1f56ae8b61f30a6876b13d2396dc1b
chaud1024 <chaud1024@gmail.com>
2024-03-15 11:23:39 +0900

    add shared components

---
+++ src/components/shared/ListRow.tsx
@@ -0,0 +1,110 @@
+import { css, SerializedStyles } from '@emotion/react'
+import React from 'react'
+import Flex from './Flex'
+import Skeleton from './Skeleton'
+import Spacing from './Spacing'
+import Text from './Text'
+
+interface ListRowProps {
+  left?: React.ReactNode
+  contents: React.ReactNode
+  right?: React.ReactNode
+  withArrow?: boolean
+  onClick?: () => void
+  as?: 'div' | 'li'
+  style?: SerializedStyles
+}
+
+const ListRow = ({
+  left,
+  contents,
+  right,
+  withArrow,
+  onClick,
+  as = 'li',
+  style,
+}: ListRowProps) => {
+  return (
+    <Flex
+      as={as}
+      css={[listRowContainerStyles, style]}
+      onClick={onClick}
+      align="center"
+    >
+      {left && <Flex css={listRowLeftStyles}>{left}</Flex>}
+      <Flex css={listRowContentStyles}>{contents}</Flex>
+      {right && <Flex>{right}</Flex>}
+      {withArrow ? <IconArrowRight /> : null}
+    </Flex>
+  )
+}
+
+const listRowContainerStyles = css`
+  padding: 8px 24px;
+`
+
+const listRowLeftStyles = css`
+  margin-right: 14px;
+`
+
+const listRowContentStyles = css`
+  flex: 1;
+`
+
+function ListRowTexts({
+  title,
+  subTitle,
+}: {
+  title: React.ReactNode
+  subTitle: React.ReactNode
+}) {
+  return (
+    <Flex direction="column">
+      <Text bold={true}>{title}</Text>
+      <Text typography="t7">{subTitle}</Text>
+    </Flex>
+  )
+}
+
+function ListRowSkeleton() {
+  return (
+    <Flex as="li" css={listRowContainerStyles} align="center">
+      <Flex css={listRowLeftStyles}></Flex>
+      <Flex css={listRowContentStyles}>
+        <ListRow.Texts
+          title={
+            <>
+              <Skeleton width={67} height={23} />
+              <Spacing size={2} />
+            </>
+          }
+          subTitle={<Skeleton width={85} height={20} />}
+        />
+      </Flex>
+      <IconArrowRight />
+    </Flex>
+  )
+}
+
+function IconArrowRight() {
+  return (
+    <svg
+      fill="none"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  )
+}
+
+ListRow.Texts = ListRowTexts // ListRow의 Texts를 사용하겠다
+ListRow.Skeleton = ListRowSkeleton // ListRow의 Texts를 사용하겠다
+
+export default ListRow
