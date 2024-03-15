bdaa1600fb842b8e6955fb9e2bfb636f3ca285a1
chaud1024 <chaud1024@gmail.com>
2024-03-15 15:12:07 +0900

    add events type

--- src/models/hotel.ts
+++ src/models/hotel.ts
@@ -9,4 +9,12 @@ export interface Hotel {
   name: string
   price: number
   starRating: number
+  events?: {
+    name: string
+    promoEndTime?: string
+    tagThemeStyle: {
+      backgroundColor: string
+      fontColor: string
+    }
+  }
 }
