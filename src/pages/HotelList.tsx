d36f42619ce8452765d59e58476d8b5088dd159b
chaud1024 <chaud1024@gmail.com>
2024-03-15 11:25:00 +0900

    edit hotel list component

--- src/pages/HotelList.tsx
+++ src/pages/HotelList.tsx
@@ -1,10 +1,31 @@
 import useHotels from '@components/hotelList/hooks/useHotels'
+import Hotel from '@components/hotelList/Hotel'
+import Spacing from '@components/shared/Spacing'
+import Top from '@components/shared/Top'
+import { Fragment } from 'react'
 
 const HotelList = () => {
   const { data: hotels } = useHotels()
 
   console.log('hotels', hotels)
-  return <div>HotelList</div>
+  return (
+    <div>
+      <Top title="인기 호텔" subTitle="호텔부터 펜션까지 최저가" />
+
+      {hotels?.map((hotel, idx) => (
+        <Fragment key={hotel.id}>
+          <Hotel hotel={hotel} />
+          {hotels.length - 1 === idx ? null : (
+            <Spacing
+              size={8}
+              backgroundColor="gray100"
+              style={{ margin: '20px 0' }}
+            />
+          )}
+        </Fragment>
+      ))}
+    </div>
+  )
 }
 
 export default HotelList
