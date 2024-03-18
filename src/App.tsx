545b35a67171144d0353eaee7944360522888ef9
chaud1024 <chaud1024@gmail.com>
2024-03-18 12:01:47 +0900

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import styled from '@emotion/styled'
 import HotelPage from '@pages/Hotel'
 import HotelList from '@pages/HotelList'
 import TestPage from '@pages/Test'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HotelList />} />
-        <Route path="/hotel/:id" element={<HotelPage />} />
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Wrap>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HotelList />} />
+          <Route path="/hotel/:id" element={<HotelPage />} />
+          <Route path="/test" element={<TestPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Wrap>
   )
 }
+
+const Wrap = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+`
 
 export default App
