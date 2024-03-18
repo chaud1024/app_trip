b83c78f25d9f6758faa9d9f47d42bd2e61e51389
chaud1024 <chaud1024@gmail.com>
2024-03-18 16:31:55 +0900

    add RecommendHotelButton

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,10 +1,12 @@
 import HotelListAddButton from '@components/test/HotelListAddButton'
+import RecommendHotelButton from '@components/test/RecommendHotelButton'
 
 const TestPage = () => {
   return (
     <div>
       TestPage
       <HotelListAddButton />
+      <RecommendHotelButton />
     </div>
   )
 }
