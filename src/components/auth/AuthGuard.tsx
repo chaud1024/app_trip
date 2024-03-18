b55e4f624043acb4d32b3697bf613ee90a22af61
chaud1024 <chaud1024@gmail.com>
2024-03-18 22:05:52 +0900

    create AuthGuard to handle user state

---
+++ src/components/auth/AuthGuard.tsx
@@ -0,0 +1,34 @@
+import { onAuthStateChanged } from 'firebase/auth'
+import React, { useState } from 'react'
+import { useSetRecoilState } from 'recoil'
+
+import { auth } from '@remote/firebase'
+import { userAtom } from '@store/atom/user'
+
+const AuthGuard = ({ children }: { children: React.ReactNode }) => {
+  const [initialize, setInitialize] = useState(false)
+  const setUser = useSetRecoilState(userAtom)
+
+  // 유저인증상태정보변화 감지
+  onAuthStateChanged(auth, (user) => {
+    console.log('hi')
+    if (user == null) {
+      setUser(null)
+    } else {
+      setUser({
+        uid: user.uid,
+        email: user.email ?? '',
+        displayName: user.displayName ?? '',
+        photoURL: user.photoURL ?? '',
+      })
+    }
+    setInitialize(true)
+  })
+
+  if (initialize === false) {
+    return null
+  }
+  return <>{children}</>
+}
+
+export default AuthGuard
