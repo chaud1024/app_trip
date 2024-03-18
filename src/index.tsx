782e4dcb82962e73f92ebd7bbfb1941520bf03d5
chaud1024 <chaud1024@gmail.com>
2024-03-18 22:06:42 +0900

    add RecoilRoot to wrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { Global } from '@emotion/react'
 import globalStyles from '@styles/globalStyles'
+import { RecoilRoot } from 'recoil'
 
 const client = new QueryClient({
   defaultOptions: {
@@ -19,9 +20,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Global styles={globalStyles} />
-    <QueryClientProvider client={client}>
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
 )
 
