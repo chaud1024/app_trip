f51325d2471013a3d2d2e8fff9c8b827166ce12a
chaud1024 <chaud1024@gmail.com>
2024-03-20 14:42:18 +0900

    create PrivateRoute for signin user

---
+++ src/components/auth/PrivateRoute.tsx
@@ -0,0 +1,14 @@
+import useUser from '@hooks/auth/useUser'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
+  const user = useUser()
+
+  if (user == null) {
+    return <Navigate to="/signin" replace={true} />
+  }
+
+  return <div>{children}</div>
+}
+
+export default PrivateRoute
