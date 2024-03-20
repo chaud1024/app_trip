92bc823ba4e21cd86457ba1af824b1aa57f73aaa
chaud1024 <chaud1024@gmail.com>
2024-03-20 14:42:25 +0900

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import styled from '@emotion/styled'
 import HotelPage from '@pages/Hotel'
 import HotelList from '@pages/HotelList'
 import MyPage from '@pages/My'
+import SettingsPage from '@pages/settings'
+import LikePage from '@pages/settings/like'
 import SigninPage from '@pages/Signin'
 import TestPage from '@pages/Test'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import AuthGuard from '@components/auth/AuthGuard'
+import PrivateRoute from '@components/auth/PrivateRoute'
 import useLoadKakao from '@hooks/useLoadKakao'
 import NavBar from '@shared/NavBar'
 
@@ -20,8 +23,31 @@ function App() {
           <Routes>
             <Route path="/" element={<HotelList />} />
             <Route path="/hotel/:id" element={<HotelPage />} />
-            <Route path="/my" element={<MyPage />} />
+            <Route
+              path="/my"
+              element={
+                <PrivateRoute>
+                  <MyPage />
+                </PrivateRoute>
+              }
+            />
             <Route path="/signin" element={<SigninPage />} />
+            <Route
+              path="/settings"
+              element={
+                <PrivateRoute>
+                  <SettingsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/settings/like"
+              element={
+                <PrivateRoute>
+                  <LikePage />
+                </PrivateRoute>
+              }
+            />
             <Route path="/test" element={<TestPage />} />
           </Routes>
         </AuthGuard>
