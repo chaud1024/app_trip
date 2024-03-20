3313494b56b430a8426efe90bed81a942173c4ac
chaud1024 <chaud1024@gmail.com>
2024-03-20 14:15:31 +0900

    update function useLike to add mutation for save/like

--- src/hooks/like/useLike.ts
+++ src/hooks/like/useLike.ts
@@ -1,9 +1,15 @@
+import { useAlertContext } from '@/context/AlertContext'
 import useUser from '@hooks/auth/useUser'
-import { getLikes } from '@remote/like'
-import { useQuery } from 'react-query'
+import { Hotel } from '@models/hotel'
+import { getLikes, toggleLike } from '@remote/like'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 
 function useLike() {
   const user = useUser()
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
+  const client = useQueryClient()
 
   const { data } = useQuery(
     ['likes'],
@@ -13,7 +19,40 @@ function useLike() {
     },
   )
 
-  return { data }
+  const { mutate } = useMutation(
+    ({ hotel }: { hotel: Pick<Hotel, 'name' | 'id' | 'mainImageUrl'> }) => {
+      if (user == null) {
+        throw new Error('로그인필요')
+      }
+      return toggleLike({ hotel, userId: user?.uid })
+    },
+    {
+      onSuccess: () => {
+        client.invalidateQueries(['likes'])
+      },
+      onError: (e: Error) => {
+        if (e.message === '로그인필요') {
+          open({
+            title: '로그인이 필요한 기능입니다.',
+            onButtonClick: () => {
+              navigate('/signin')
+            },
+          })
+
+          return
+        }
+
+        open({
+          title: '알 수 없는 에러가 발생했습니다. 잠시 후 다시 시도해주세요.',
+          onButtonClick: () => {
+            // 다른 액션
+          },
+        })
+      },
+    },
+  )
+
+  return { data, mutate }
 }
 
 export default useLike
