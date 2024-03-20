279ac290e93913680ddc16fe44260bca6eb9d166
chaud1024 <chaud1024@gmail.com>
2024-03-20 16:20:38 +0900

    add updatedLikesSave function

--- src/components/settings/like/hooks/useEditLike.ts
+++ src/components/settings/like/hooks/useEditLike.ts
@@ -1,15 +1,28 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { useQueryClient } from 'react-query'
 
+import { useAlertContext } from '@context/AlertContext'
 import useLike from '@hooks/like/useLike'
 import { Like } from '@models/like'
+import { updateOrder } from '@remote/like'
 
 function useEditLike() {
-  const { data = [] } = useLike()
+  const { data } = useLike()
   const [updatedLikes, setUpdatedLikes] = useState<Like[]>([]) // drag&drop을 통해 변화있는 like 배열 구하기
+  const [isEdit, setIsEdit] = useState(false)
+  const { open } = useAlertContext()
+  const client = useQueryClient()
 
-  const reorder = useCallback(
-    (from: number, to: number) => {
-      const newItems = [...data]
+  useEffect(() => {
+    if (data != null) {
+      setUpdatedLikes(data)
+    }
+  }, [data])
+
+  const reorder = useCallback((from: number, to: number) => {
+    setIsEdit(true)
+    setUpdatedLikes((prevUpdatedLikes) => {
+      const newItems = [...prevUpdatedLikes]
 
       const [fromItem] = newItems.splice(from, 1)
 
@@ -17,14 +30,35 @@ function useEditLike() {
         newItems.splice(to, 0, fromItem)
       }
 
-      setUpdatedLikes(newItems)
-    },
-    [data],
-  )
+      newItems.forEach((like, idx) => {
+        like.order = idx + 1
+      })
 
-  const isEdit = updatedLikes.length > 0
+      return newItems
+    })
+  }, [])
 
-  return { data: isEdit ? updatedLikes : data, isEdit, reorder }
+  const updatedLikesSave = async () => {
+    try {
+      await updateOrder(updatedLikes)
+      client.setQueriesData(['likes'], updatedLikes) // 캐시된 데이터를 바로 바꾸면서 곧장 업데이트된 리스트 화면 구현
+      setIsEdit(false)
+    } catch (e) {
+      open({
+        title: '알 수 없는 에러가 발생했습니다. 잠시 후 다시 시도해주세요.',
+        onButtonClick: () => {
+          setIsEdit(false)
+        },
+      })
+    }
+  }
+
+  return {
+    data: isEdit ? updatedLikes : data,
+    isEdit,
+    reorder,
+    updatedLikesSave,
+  }
 }
 
 export default useEditLike
