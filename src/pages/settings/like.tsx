88cdc4ab5f86f7088608622081df2447b2297fdb
chaud1024 <chaud1024@gmail.com>
2024-03-20 15:21:20 +0900

    add drag&drop layout by using react-beautiful-dnd

--- src/pages/settings/like.tsx
+++ src/pages/settings/like.tsx
@@ -1,5 +1,67 @@
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DroppableProps,
+} from 'react-beautiful-dnd'
+
+import { useEffect, useState } from 'react'
+import ListRow from '@components/shared/ListRow'
+
 const LikePage = () => {
-  return <div>LikePage</div>
+  const data = [] // 찜한 호텔아이템 목록
+  return (
+    <div>
+      <DragDropContext onDragEnd={() => {}}>
+        <StrictModeDroppable droppableId="likes">
+          {(droppableProps) => (
+            <ul
+              ref={droppableProps.innerRef}
+              {...droppableProps.droppableProps}
+            >
+              {data?.map(() => {
+                return (
+                  <Draggable key={} draggableId={} index={}>
+                    {(draggableProps) => (
+                      <li
+                        ref={draggableProps.innerRef}
+                        {...draggableProps.draggableProps}
+                        {...draggableProps.dragHandleProps}
+                      >
+                        <ListRow
+                          as="div"
+                          contents={<ListRow.Texts title={} subTitle={} />}
+                        />
+                      </li>
+                    )}
+                  </Draggable>
+                )
+              })}
+            </ul>
+          )}
+        </StrictModeDroppable>
+      </DragDropContext>
+    </div>
+  )
+}
+
+function StrictModeDroppable({ children, ...props }: DroppableProps) {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true))
+
+    return () => {
+      cancelAnimationFrame(animation)
+      setEnabled(false)
+    }
+  }, [])
+
+  if (enabled === false) {
+    return null
+  }
+
+  return <Droppable {...props}>{children}</Droppable>
 }
 
 export default LikePage
