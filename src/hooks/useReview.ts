e3261ddc1be68160f1e485ce639d4893add0f077
chaud1024 <chaud1024@gmail.com>
2024-03-22 08:32:38 +0900

    add new review into reviews data & invalidate queries to get a new list of reviews

--- src/hooks/useReview.ts
+++ src/hooks/useReview.ts
@@ -1,13 +1,37 @@
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 
-import { getReviews } from '@remote/review'
+import useUser from '@hooks/auth/useUser'
+import { getReviews, writeReview } from '@remote/review'
 
 function useReview({ hotelId }: { hotelId: string }) {
+  const user = useUser()
+  const client = useQueryClient()
+
   const { data, isLoading } = useQuery(['reviews', hotelId], () =>
     getReviews({ hotelId }),
   )
 
-  return { data, isLoading }
+  const { mutateAsync: write } = useMutation(
+    async (text: string) => {
+      const newReview = {
+        createdAt: new Date(),
+        hotelId,
+        userId: user?.uid as string,
+        text,
+      }
+      await writeReview(newReview)
+
+      return true
+    },
+    {
+      onSuccess: () => {
+        // 새 리뷰가 추가되었다면 기존 reviews데이터를 업데이트(쿼리 갱신)
+        client.invalidateQueries(['reviews', hotelId])
+      },
+    },
+  )
+
+  return { data, isLoading, write }
 }
 
 export default useReview
