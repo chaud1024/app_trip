90c9a2a5891af555ef410955e7176f811fc17127
chaud1024 <chaud1024@gmail.com>
2024-03-21 17:48:53 +0900

    update typo

--- src/hooks/useReview.ts
+++ src/hooks/useReview.ts
@@ -3,9 +3,9 @@ import { useQuery } from 'react-query'
 import { getReviews } from '@remote/review'
 
 function useReview({ hotelId }: { hotelId: string }) {
-  const { data, isLoading } = useQuery(['reviews', hotelId], () => {
-    getReviews({ hotelId })
-  })
+  const { data, isLoading } = useQuery(['reviews', hotelId], () =>
+    getReviews({ hotelId }),
+  )
 
   return { data, isLoading }
 }
