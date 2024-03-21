d35b868086099a759a11f84f210d33e130547c9b
chaud1024 <chaud1024@gmail.com>
2024-03-21 17:49:14 +0900

    add reviews list

--- src/components/hotel/Review.tsx
+++ src/components/hotel/Review.tsx
@@ -1,19 +1,29 @@
+import useUser from '@hooks/auth/useUser'
 import useReview from '@hooks/useReview'
-
-import Text from '@shared/Text'
+import { format } from 'date-fns'
 import { useCallback } from 'react'
-import Flex from '../shared/Flex'
-import Spacing from '../shared/Spacing'
+
+import Button from '@shared/Button'
+import Flex from '@shared/Flex'
+import ListRow from '@shared/ListRow'
+import Spacing from '@shared/Spacing'
+import Text from '@shared/Text'
 
 const Review = ({ hotelId }: { hotelId: string }) => {
+  const user = useUser()
   const { data: reviews, isLoading } = useReview({ hotelId })
 
   const reviewRows = useCallback(() => {
     if (reviews?.length === 0) {
       return (
         <div>
-          <Flex>
-            <img src="" alt="" />
+          <Flex direction="column" align="center" style={{ padding: '40px 0' }}>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/business-development-6/24/Document_business_paper_file_paperwork_job-512.png"
+              alt="review"
+              width={60}
+              height={60}
+            />
             <Spacing size={10} />
             <Text typography="t6">
               아직 작성된 리뷰가 없습니다. 첫 리뷰를 작성해보세요!
@@ -22,7 +32,28 @@ const Review = ({ hotelId }: { hotelId: string }) => {
         </div>
       )
     }
-  }, [reviews])
+
+    return (
+      <ul>
+        {reviews?.map((review) => (
+          <ListRow
+            left={
+              review.user.photoURL != null ? (
+                <img src={review.user.photoURL} alt="" width={40} height={40} />
+              ) : null
+            }
+            contents={
+              <ListRow.Texts
+                title={review.text}
+                subTitle={format(review.createdAt, 'yyyy-MM-dd')}
+              />
+            }
+            right={review.userId === user?.uid ? <Button>삭제</Button> : null}
+          />
+        ))}
+      </ul>
+    )
+  }, [reviews, user])
 
   if (isLoading === true) {
     return null
@@ -33,6 +64,9 @@ const Review = ({ hotelId }: { hotelId: string }) => {
       <Text bold={true} typography="t4" style={{ padding: '0 24px' }}>
         리뷰
       </Text>
+      <Spacing size={16} />
+      {reviewRows()}
+      {}
     </div>
   )
 }
