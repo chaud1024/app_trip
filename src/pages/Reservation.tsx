5f7495b9a91b08c6ad63f3df0a5f93d6d88c7ade
chaud1024 <chaud1024@gmail.com>
2024-03-23 00:12:28 +0900

    add logic of handleSumit by using makeReservation

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -4,10 +4,15 @@ import { useEffect } from 'react'
 import Form from '@components/reservation/Form'
 import useReservation from '@components/reservation/hooks/useReservation'
 import Summary from '@components/reservation/Summary'
+import useUser from '@hooks/auth/useUser'
 import Spacing from '@shared/Spacing'
-import addDelimiter from '@/utils/addDelimiter'
+import addDelimiter from '@utils/addDelimiter'
+import { useNavigate } from 'react-router-dom'
 
 const ReservationPage = () => {
+  const user = useUser()
+  const navigate = useNavigate()
+
   const { hotelId, roomId, startDate, endDate, nights } = parse(
     window.location.search,
     { ignoreQueryPrefix: true },
@@ -21,15 +26,18 @@ const ReservationPage = () => {
 
   useEffect(() => {
     if (
-      [hotelId, roomId, startDate, endDate, nights].some((param) => {
+      [user, hotelId, roomId, startDate, endDate, nights].some((param) => {
         return param == null
       })
     ) {
       window.history.back()
     }
-  }, [hotelId, roomId, startDate, endDate, nights])
+  }, [user, hotelId, roomId, startDate, endDate, nights])
 
-  const { data, isLoading } = useReservation({ hotelId, roomId })
+  const { data, isLoading, makeReservation } = useReservation({
+    hotelId,
+    roomId,
+  })
 
   if (data == null || isLoading === true) {
     return null
@@ -37,7 +45,20 @@ const ReservationPage = () => {
 
   const { hotel, room } = data
 
-  const handleSubmit = () => {}
+  const handleSubmit = async (formValues: { [key: string]: string }) => {
+    const newReservation = {
+      userId: user?.uid as string,
+      hotelId,
+      roomId,
+      startDate,
+      price: room.price * Number(nights),
+      formValues,
+    }
+
+    await makeReservation(newReservation)
+
+    navigate(`/reservation/done?hotelName=${hotel.name}`)
+  }
 
   const buttonLabel = `${nights}박 ${addDelimiter(room.price * Number(nights))}원 예약하기`
 
