5e060258674947359cdc0da0d869a2122c6b8de9
chaud1024 <chaud1024@gmail.com>
2024-03-22 17:36:30 +0900

    create function useReservation to get room data by using getHotelWithRoom

---
+++ src/components/reservation/hooks/useReservation.ts
@@ -0,0 +1,33 @@
+import { useAlertContext } from '@context/AlertContext'
+import { getHotelWithRoom } from '@remote/hotel'
+import { useQuery } from 'react-query'
+
+function useReservation({
+  hotelId,
+  roomId,
+}: {
+  hotelId: string
+  roomId: string
+}) {
+  const { open } = useAlertContext()
+  const { data, isLoading } = useQuery(
+    ['hotelWithRoom', hotelId, roomId],
+    () => getHotelWithRoom({ hotelId, roomId }),
+    {
+      onSuccess: ({ room }) => {
+        if (room.avaliableCount === 0) {
+          open({
+            title: '객실이 매진되었습니다.',
+            onButtonClick: () => {
+              window.history.back()
+            },
+          })
+        }
+      },
+    },
+  )
+
+  return { data, isLoading }
+}
+
+export default useReservation
