3dcfd87904507e88d84673877a1386e7a5357468
chaud1024 <chaud1024@gmail.com>
2024-03-23 00:11:22 +0900

    create function makeReservation to update data to firebase

---
+++ src/remote/reservation.ts
@@ -0,0 +1,28 @@
+import { collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
+import { store } from './firebase'
+
+import { COLLECTIONS } from '@constants'
+import { Reservation } from '@models/reservation'
+import { Room } from '@models/room'
+
+export async function makeReservation(newReservation: Reservation) {
+  const hotelSnapshot = doc(store, COLLECTIONS.HOTEL, newReservation.hotelId)
+  const roomSnapshot = await getDoc(
+    doc(hotelSnapshot, COLLECTIONS.ROOM, newReservation.roomId),
+  )
+
+  const room = roomSnapshot.data() as Room
+  const 현재잔여객실수 = room.avaliableCount
+
+  if (현재잔여객실수 === 0) {
+    throw new Error('no room')
+  }
+
+  return Promise.all([
+    updateDoc(roomSnapshot.ref, {
+      avaliableCount: Number(현재잔여객실수) - 1,
+    }),
+
+    setDoc(doc(collection(store, COLLECTIONS.RESERVATION)), newReservation),
+  ])
+}
